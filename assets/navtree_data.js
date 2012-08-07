acd80bd597e8bf0da40e4caa20b4a8aa91551891
johndemic <john.demic@gmail.com>
2012-08-06 20:16:22 -0700

    Updating documentation

---
+++ assets/navtree_data.js
@@ -0,0 +1,17 @@
+var NAVTREE_DATA =
+[ [ "org.mule.module.jpa", "org/mule/module/jpa/package-summary.html", [ [ "Classes", null, [ [ "JPAModule", "org/mule/module/jpa/JPAModule.html", null, "" ], [ "JPATransaction", "org/mule/module/jpa/JPATransaction.html", null, "" ], [ "JPATransactionFactory", "org/mule/module/jpa/JPATransactionFactory.html", null, "" ], [ "JPAUtils", "org/mule/module/jpa/JPAUtils.html", null, "" ], [ "MuleEntityManager", "org/mule/module/jpa/MuleEntityManager.html", null, "" ], [ "PersistenceContextProcessor", "org/mule/module/jpa/PersistenceContextProcessor.html", null, "" ] ]
+, "" ], [ "Exceptions", null, [ [ "JPAException", "org/mule/module/jpa/JPAException.html", null, "" ] ]
+, "" ] ]
+, "" ], [ "org.mule.module.jpa.adapters", "org/mule/module/jpa/adapters/package-summary.html", [ [ "Classes", null, [ [ "JPAModuleCapabilitiesAdapter", "org/mule/module/jpa/adapters/JPAModuleCapabilitiesAdapter.html", null, "" ], [ "JPAModuleLifecycleAdapter", "org/mule/module/jpa/adapters/JPAModuleLifecycleAdapter.html", null, "" ] ]
+, "" ] ]
+, "" ], [ "org.mule.module.jpa.command", "org/mule/module/jpa/command/package-summary.html", [ [ "Interfaces", null, [ [ "JPACommand", "org/mule/module/jpa/command/JPACommand.html", null, "" ] ]
+, "" ], [ "Classes", null, [ [ "Detach", "org/mule/module/jpa/command/Detach.html", null, "" ], [ "Find", "org/mule/module/jpa/command/Find.html", null, "" ], [ "Merge", "org/mule/module/jpa/command/Merge.html", null, "" ], [ "Persist", "org/mule/module/jpa/command/Persist.html", null, "" ], [ "Query", "org/mule/module/jpa/command/Query.html", null, "" ] ]
+, "" ] ]
+, "" ], [ "org.mule.module.jpa.config", "org/mule/module/jpa/config/package-summary.html", [ [ "Classes", null, [ [ "DetachDefinitionParser", "org/mule/module/jpa/config/DetachDefinitionParser.html", null, "" ], [ "FindDefinitionParser", "org/mule/module/jpa/config/FindDefinitionParser.html", null, "" ], [ "JPAModuleConfigDefinitionParser", "org/mule/module/jpa/config/JPAModuleConfigDefinitionParser.html", null, "" ], [ "JPAModuleNamespaceHandler", "org/mule/module/jpa/config/JPAModuleNamespaceHandler.html", null, "" ], [ "MergeDefinitionParser", "org/mule/module/jpa/config/MergeDefinitionParser.html", null, "" ], [ "PersistDefinitionParser", "org/mule/module/jpa/config/PersistDefinitionParser.html", null, "" ], [ "QueryDefinitionParser", "org/mule/module/jpa/config/QueryDefinitionParser.html", null, "" ] ]
+, "" ] ]
+, "" ], [ "org.mule.module.jpa.processors", "org/mule/module/jpa/processors/package-summary.html", [ [ "Classes", null, [ [ "DetachMessageProcessor", "org/mule/module/jpa/processors/DetachMessageProcessor.html", null, "" ], [ "FindMessageProcessor", "org/mule/module/jpa/processors/FindMessageProcessor.html", null, "" ], [ "MergeMessageProcessor", "org/mule/module/jpa/processors/MergeMessageProcessor.html", null, "" ], [ "PersistMessageProcessor", "org/mule/module/jpa/processors/PersistMessageProcessor.html", null, "" ], [ "QueryMessageProcessor", "org/mule/module/jpa/processors/QueryMessageProcessor.html", null, "" ] ]
+, "" ] ]
+, "" ] ]
+
+;
+
